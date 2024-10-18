3fcb124918497235c63f6b8d61ed60c8a9d8b590
md_shemil <shemil1625@gmail.com>
2024-10-18 09:28:32 +0530

    final commit

---
+++ src/main/java/com/FinSight/entity/Transaction.java
@@ -0,0 +1,71 @@
+package com.FinSight.entity;
+import java.lang.Double;
+import javax.persistence.*;
+
+@Entity
+public class Transaction {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id;
+
+	private String transactionType; // Added to capture the transaction type
+	private String category;        // Changed from name to category
+	private String note;
+	private Double price;
+	      // Changed from amount to price
+
+	public Transaction(int id, String transactionType, String category, String note, Double price) {
+		super();
+		this.id = id;
+		this.transactionType = transactionType;
+		this.category = category;
+		this.note = note;
+		this.price = price;
+	}
+
+	public Transaction() {
+		super();
+		// Default constructor
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getTransactionType() {
+		return transactionType;
+	}
+
+	public void setTransactionType(String transactionType) {
+		this.transactionType = transactionType;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getNote() {
+		return note;
+	}
+
+	public void setNote(String note) {
+		this.note = note;
+	}
+
+	public Double getPrice() {
+		return price;
+	}
+
+	public void setPrice(Double price) {
+		this.price = price;
+	}
+}
